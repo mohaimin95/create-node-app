34b09542a4bfd8125ac4db440bda4ae3a146593f
unknown <mohaimin95@gmail.com>
2020-12-29 14:48:20 +0530

    added extra validations and indexing for email and name

--- app/models/User.js
+++ app/models/User.js
@@ -5,13 +5,20 @@ const User = new mongoose.Schema({
     name: {
         required: true,
         type: String,
+        trim:true,
         min: 3
     },
     email: {
         required: true,
         type: String,
-        min: 6,
-        unique: true
+        unique: true,
+        lowercase:true,
+        trim:true,
+        index:true,
+        validate:{
+            validator:(v) => validateEmail(v),
+            message:"Please enter an valid email."
+        }
     },
     password: {
         required: true,
@@ -24,4 +31,9 @@ const User = new mongoose.Schema({
     }
 }, { timestamps: true });
 
+
+function validateEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
 module.exports = mongoose.model('user',User);
