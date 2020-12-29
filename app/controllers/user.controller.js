387220453f8f3118d890a8d8edede1211f3531db
unknown <mohaimin95@gmail.com>
2020-12-29 11:11:31 +0530

    added user profile route

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -1,74 +1,83 @@
 const User = require("../models/User");
 const bcrypt = require("../helpers/bcrypt");
 const jwt = require("../helpers/jwt");
-
+const { ObjectId } = require("mongoose").Types;
 let userController = {};
 
 userController.register = ({ name, email, password }) => {
-    return new Promise((resolve, reject) => {
-        const user = new User({
-            name: name.trim(),
-            email: email.toLowerCase().trim(),
-            password: bcrypt.generateHash(password),
-
-        });
-        user.save((err, doc) => {
-            if (err) {
-                let message = "Error in Saving Data !", status = 500;
-                switch (String(err.code)) {
-                    case '11000':
-                        message = "Email Already Exists";
-                        status = 409
-                        break;
-                }
-                reject({
-                    message,
-                    status
-                })
-            } else {
-                resolve({
-                    message: "Registration success !",
-                    token: generateToken(doc._id)
-                });
-            }
-        });
+  return new Promise((resolve, reject) => {
+    const user = new User({
+      name: name.trim(),
+      email: email.toLowerCase().trim(),
+      password: bcrypt.generateHash(password),
     });
-
-}
-userController.login = ({ email, password }) => {
-    return new Promise((resolve, reject) => {
-        User.findOne({ 'email': email.trim().toLowerCase() }, (err, user) => {
-            if (err) {
-                return reject({
-                    status: 500,
-                    message: "Error ! Try Again.",
-                    error: err
-                })
-            } else {
-                if (!user) {
-                    return reject({
-                        status: 401,
-                        message: "Unauthorized user !"
-                    })
-                } else {
-                    if (bcrypt.validate(password, user.password)) {
-                        return resolve({
-                            token: generateToken(user._id)
-                        })
-                    } else {
-                        return reject({
-                            status: 401,
-                            message: "Unauthorized user !"
-                        })
-                    }
-
-                }
-            }
+    user.save((err, doc) => {
+      if (err) {
+        let message = "Error in Saving Data !",
+          status = 500;
+        switch (String(err.code)) {
+          case "11000":
+            message = "Email Already Exists";
+            status = 409;
+            break;
+        }
+        reject({
+          message,
+          status,
         });
-    })
-}
+      } else {
+        resolve({
+          message: "Registration success !",
+          token: generateToken(doc._id),
+        });
+      }
+    });
+  });
+};
+userController.login = ({ email, password }) => {
+  return new Promise((resolve, reject) => {
+    User.findOne({ email: email.trim().toLowerCase() }, (err, user) => {
+      if (err) {
+        return reject({
+          status: 500,
+          message: "Error ! Try Again.",
+          error: err,
+        });
+      } else {
+        if (!user) {
+          return reject({
+            status: 401,
+            message: "Unauthorized user !",
+          });
+        } else {
+          if (bcrypt.validate(password, user.password)) {
+            return resolve({
+              token: generateToken(user._id),
+            });
+          } else {
+            return reject({
+              status: 401,
+              message: "Unauthorized user !",
+            });
+          }
+        }
+      }
+    });
+  });
+};
 userController.changePassword = (password, userId) => {
-    return User.updateOne({ _id: userId }, { $set: { password: bcrypt.generateHash(password) } });
-}
-const generateToken = _id => jwt.generateToken({ _id, createdAt: new Date().getTime() })
+  return User.updateOne(
+    { _id: userId },
+    { $set: { password: bcrypt.generateHash(password) } }
+  );
+};
+userController.getUserById = (userId) => {
+  if (ObjectId.isValid(userId)) {
+    return User.findById(userId, { password: 0,__v:0 }).lean();
+  } else {
+    return Promise.resolve(null);
+  }
+};
+const generateToken = (_id) =>
+  jwt.generateToken({ _id, createdAt: new Date().getTime() });
 module.exports = userController;
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,54 +1,65 @@
 const router = require("express").Router();
 const userController = require("../controllers/user.controller");
 const Auth = require("../middlewares/auth");
-router.post('/register', (req, res) => {
-    let {
-        email = null,
-        password = null,
-        name = null
-    } = req.body;
-    if (email && password && name && validateEmail(email)) {
-        userController.register({
-            email,
-            password,
-            name,
-        }).then(data => res.send(data))
-            .catch(err => {
-                res.status(err.status || 500).send({ message: err.message || "error" })
-            });
-    } else {
-        res.status(400).send({
-            message: "Invalid/Missing email or password !"
-        })
-    }
+router.post("/register", (req, res) => {
+  let { email = null, password = null, name = null } = req.body;
+  if (email && password && name && validateEmail(email)) {
+    userController
+      .register({
+        email,
+        password,
+        name,
+      })
+      .then((data) => res.send(data))
+      .catch((err) => {
+        res.status(err.status || 500).send({ message: err.message || "error" });
+      });
+  } else {
+    res.status(400).send({
+      message: "Invalid/Missing email or password !",
+    });
+  }
 });
 
-router.post('/login', (req, res) => {
-    if (req.body.email && req.body.password) {
-        userController.login({
-            email: req.body.email,
-            password: req.body.password
-        }).then(data => res.send(data))
-            .catch(err => res.status(err.status || 500).send(err));
-    } else {
-        res.status(400).send({
-            message: "Invalid/Missing email or password !"
-        })
-    }
-});
-router.post('/change-password', Auth(), (req, res) => {
-    if (req.body.password) {
-        userController.changePassword(req.body.password, req.user._id).then(() => res.send({ message: "Successfully Changed !" }))
-            .catch(err => res.status(err.status || 500).send(err));
-    } else {
-        res.status(400).send({
-            message: "Invalid/Missing Password"
-        })
-    }
+router.post("/login", (req, res) => {
+  if (req.body.email && req.body.password) {
+    userController
+      .login({
+        email: req.body.email,
+        password: req.body.password,
+      })
+      .then((data) => res.send(data))
+      .catch((err) => res.status(err.status || 500).send(err));
+  } else {
+    res.status(400).send({
+      message: "Invalid/Missing email or password !",
+    });
+  }
 });
 
+//================== User Authentication needed ==========================================
+router.use(Auth());
+router.post("/change-password", (req, res) => {
+  if (req.body.password) {
+    userController
+      .changePassword(req.body.password, req.user._id)
+      .then(() => res.send({ message: "Successfully Changed !" }))
+      .catch((err) => res.status(err.status || 500).send(err));
+  } else {
+    res.status(400).send({
+      message: "Invalid/Missing Password",
+    });
+  }
+});
+router.get("/", (req, res) => {
+  let { _id: userId, createdAt } = req.user;
+  userController
+    .getUserById(userId)
+    .then((data) => res.send({ ...data, loggedInAt: createdAt }))
+    .catch((err) => res.status(500).send(err));
+});
 const validateEmail = (email) => {
-    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
+  var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
 module.exports = router;
